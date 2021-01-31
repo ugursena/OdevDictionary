b93aa307e6b1d597bdec3aedd9a43f0097e8e1f0
Sena Uğur <senauguur@gmail.com>
2021-01-31 19:09:06 +0300

    Add project files.

---
+++ OdevDictionary/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdevDictionary
+{
+    class MyDictionary<T, V>
+    {
+        T[] keyArray;
+        V[] valueArray;
+
+        public MyDictionary()
+        {
+            keyArray = new T[0];
+            valueArray = new V[0];
+
+        }
+        public void Add(T key, V value)
+        {
+            T[] tempKey = keyArray;
+            V[] tempValue = valueArray;
+            keyArray = new T[keyArray.Length + 1];
+            valueArray = new V[valueArray.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keyArray[i] = tempKey[i];
+            }
+            keyArray[keyArray.Length - 1] = key;
+            for (int j = 0; j < tempValue.Length; j++)
+            {
+                valueArray[j] = tempValue[j];
+            }
+            valueArray[valueArray.Length - 1] = value;
+
+            Console.WriteLine("Id : " + key + " Name : " + value);
+        }
+    }
+}
---
+++ OdevDictionary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OdevDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(1, "zxcv");
+        }
+    }
+}
